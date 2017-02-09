f893e957a2e54a9ae275af8ec3ff3b55d7d76fa0
andra <andra@Andra>
2017-02-09 21:19:47 +0200

    Hello

--- JavaHomeworksAndraMariaPuscas/src/javahomeworksandramariapuscas/JavaHomeworksAndraMariaPuscas.java
+++ JavaHomeworksAndraMariaPuscas/src/javahomeworksandramariapuscas/JavaHomeworksAndraMariaPuscas.java
@@ -15,7 +15,7 @@ public class JavaHomeworksAndraMariaPuscas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("helo");
+        System.out.println("hello");
     }
     
 }

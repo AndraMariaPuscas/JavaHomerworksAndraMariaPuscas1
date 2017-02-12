adb550cf49fab9855036e0a3e328d10a83a0c012
andra <andra@Andra>
2017-02-12 16:05:31 +0200

    input from keyboard

--- JavaHomeworksAndraMariaPuscas/src/javahomeworksandramariapuscas/JavaHomeworksAndraMariaPuscas.java
+++ JavaHomeworksAndraMariaPuscas/src/javahomeworksandramariapuscas/JavaHomeworksAndraMariaPuscas.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javahomeworksandramariapuscas;
 
 /**
  *
  * @author andra
  */
+import java.util.Scanner;
 public class JavaHomeworksAndraMariaPuscas {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello");
+       Scanner scan=new Scanner(System.in);
+       System.out.print("Enter text:");
+       String text=scan.nextLine();
+        System.out.println("The text you entered is: " + text);
     }
     
 }
